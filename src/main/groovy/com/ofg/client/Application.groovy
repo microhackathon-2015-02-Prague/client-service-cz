d382bee5924bb5471efe6fb82dccd016b66b787e
Matus.Hennel <matus.hennel@4finance.com>
2015-02-23 15:22:40 +0100

    delete twitter first draft

--- src/main/groovy/com/ofg/client/Application.groovy
+++ src/main/groovy/com/ofg/client/Application.groovy
@@ -1,11 +1,11 @@
-package com.ofg.twitter
-
+package com.ofg.client
 import com.ofg.infrastructure.environment.EnvironmentSetupVerifier
 import groovy.transform.TypeChecked
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.EnableAspectJAutoProxy
+import org.springframework.context.annotation.Import
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration
 import org.springframework.scheduling.annotation.EnableAsync
 
 import static com.ofg.config.BasicProfiles.*
@@ -13,8 +13,8 @@ import static com.ofg.config.BasicProfiles.*
 @TypeChecked
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-@EnableCaching
 @EnableAsync
+@Import(RepositoryRestMvcConfiguration.class)
 class Application {
 
     static void main(String[] args) {
---
+++ src/main/groovy/com/ofg/client/model/Person.groovy
@@ -0,0 +1,16 @@
+package com.ofg.client.model
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id
+
+    String firstName
+    String lastName
+}
