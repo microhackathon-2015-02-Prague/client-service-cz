c36b96b0ed40fd874bfeb708bf5e20b37df35a3e
Marcin Grzejszczak <marcin.grzejszczak@4finance.com>
2015-02-24 10:30:24 +0100

    Fix

--- src/test/groovy/com/ofg/twitter/places/accurest/BaseMockMvcSpec.groovy
+++ src/test/groovy/com/ofg/twitter/places/accurest/BaseMockMvcSpec.groovy
@@ -1,9 +1,14 @@
 package com.ofg.twitter.places.accurest
+
+import com.jayway.restassured.RestAssured
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc
 import com.ofg.client.Application
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.IntegrationTest
 import org.springframework.boot.test.SpringApplicationContextLoader
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.web.context.WebApplicationContext
 import spock.lang.Specification
 
 @IntegrationTest
@@ -11,8 +16,11 @@ import spock.lang.Specification
 @ContextConfiguration(classes = Application, loader = SpringApplicationContextLoader)
 abstract class BaseMockMvcSpec extends Specification {
 
-    def setup() {
+    @Autowired WebApplicationContext webApplicationContext
 
+    def setup() {
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext)
+        RestAssured.port = 9058
     }
 
 }
